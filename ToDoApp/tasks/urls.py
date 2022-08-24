bb494272fa968f2b5575df9762c7a5e6c3f785a3
VovaHayek <vova.hayek@gmail.com>
2022-08-24 10:57:22 +0300

    Implemented User's Registration and Login

--- ToDoApp/tasks/urls.py
+++ ToDoApp/tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.to_do_tasks)
+    path('', views.to_do_tasks),
+    path('loginForm/', views.loginForm)
 ]
--- ToDoApp/tasks/views.py
+++ ToDoApp/tasks/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+
+from .models import User
+
+def loginForm(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            if User.objects.filter(username=username).exists():
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                return redirect('/')
+            else:
+                user = User.objects.create_user(username, password)
+                login(request, user)
+                return redirect('/')
+    return render(request, 'includes/loginForm.html')
 
 def to_do_tasks(request):
+
     return render(request, 'ToDoApp/tasks.html')
